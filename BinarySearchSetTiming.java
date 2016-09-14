868c24cff2dfc8789bcd2b68f4d829366ee2dd8c
Nickolas Komarnitsky <komarnickolas@gmail.com>
2016-09-14 13:37:57 -0600

    update

--- BinarySearchSetTiming.java
+++ BinarySearchSetTiming.java
@@ -27,8 +27,10 @@ public class BinarySearchSetTiming extends Application {
 
 		lineChart.setTitle("BinarySearchSet timing");
 
-		XYChart.Series series = new XYChart.Series();
-		series.setName("BinarySearchSet timing data");
+		XYChart.Series series1 = new XYChart.Series();
+		XYChart.Series series2 = new XYChart.Series();
+		series1.setName("BinarySearchSet timing data: contains()");
+		series2.setName("BinarySearchSet timing data: add()");
 
 		BinarySearchSet<Double> binaryD = new BinarySearchSet<Double>();
 		double x = 0;
@@ -39,21 +41,35 @@ public class BinarySearchSetTiming extends Application {
 			x++;
 		}
 		int count = 0;
-		while (count < 100) {
+		while (count < 10000) {
 			double num = 0 + (int) (Math.random() * binaryD.size);
 			long start;
 			start = System.nanoTime();
 			binaryD.contains(num);
 			long end = System.nanoTime() - start;
 			double time = end / (double) BILLION;
-			series.getData().add(new XYChart.Data(count+1, time));
+			series1.getData().add(new XYChart.Data(count+1, time));
 			// System.out.println("BinarySearchSet.contains() took " + time +
 			// "seconds to look through " + binaryD.size()
 			// + " items for " + num);
 			count++;
 		}
+		count = 0;
+		while(count < 10000){
+			double num = 51.5;//0 + (int) (Math.random() * binaryD.size);
+			long start;
+			start = System.nanoTime();
+			binaryD.add(num);
+			long end = System.nanoTime() - start;
+			double time = end / (double) BILLION;
+			binaryD.remove(num);
+			series2.getData().add(new XYChart.Data(count+1, time));
+			count++;
+		}
 		Scene scene = new Scene(lineChart, 800, 600);
-		lineChart.getData().add(series);
+		lineChart.getData().add(series1);
+//		lineChart.getData().add(series2);
+		lineChart.setCreateSymbols(false);
 
 		stage.setScene(scene);
 		stage.show();
